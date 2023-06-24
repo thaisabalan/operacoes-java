387c3e9006cb622e48a6cd10dd75d0a2c5a685e8
thaisabalan <132139027+thaisabalan@users.noreply.github.com>
2023-06-24 11:49:39 -0300

    Add files via upload

---
+++ Operacoes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp.senai.br.calculomedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class Operacoes {
+    
+    public static Scanner teclado = new Scanner(System.in);
+    
+    public static double soma(double valor1, double valor2){
+        return valor1 + valor2;
+    }
+}
